216b924e8b1f46d9b59f1eaf05a45b2b3d60cd28
VictorMMarcelo <victormcpinheiro2@gmail.com>
2023-07-31 19:23:37 -0300

    seguir

--- pages/api/seguir.ts
+++ pages/api/seguir.ts
@@ -3,6 +3,7 @@ import type {RespostaPadraoMsg} from "@/types/RespostaPadraoMsg";
 import { validarTokenJWT } from '@/middlewares/validarTokenJWT';
 import { conectarMongoDB } from '@/middlewares/conectarMongoDB';
 import { UsuarioModel } from '@/models/UsuarioModels';
+import { SeguidorModel } from '@/models/SeguidorModel';
 
 const endpointSeguir =async (req : NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
     try{
@@ -19,6 +20,39 @@ const endpointSeguir =async (req : NextApiRequest, res: NextApiResponse<Resposta
             const usuarioASerSeguido =  await UsuarioModel.findById(id);
             if(!usuarioASerSeguido){
                 return res.status(400).json({erro :'Usuario a ser seguido nao encontrado'});
+            }
+            //inserir o registro?
+            //e se ela ja segue?
+            const euJaSigoEsseUsuario = await SeguidorModel.find({usuarioId: usuarioLogado._id, usuarioSeguidoId: usuarioASerSeguido._id});
+            if (euJaSigoEsseUsuario && euJaSigoEsseUsuario.length > 0 ){
+                //sinal que eu ja sigo esse usuario
+                euJaSigoEsseUsuario.forEach(async(e: any)=> await SeguidorModel.findByIdAndDelete({_id : e._id}));
+                usuarioLogado.seguindo--;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioLogado._id}, usuarioLogado);
+                usuarioASerSeguido.seguidores--;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioASerSeguido._id}, usuarioASerSeguido);
+                return res.status(200).json({msg: 'Deixou de seguir o usuario com sucesso'});
+
+            }else{
+                // sinal que eu nao sigo esse usuario
+                const seguidor = {
+                    usuarioId : usuarioLogado._id ,
+                    usuarioSeguidoId : usuarioASerSeguido._id
+                }
+                await SeguidorModel.create(seguidor);
+
+                //o usuario logado esta seguind um usuario novo
+                //o numeto de seguindo dele tem que aumentar
+                //adicionar um seguindo no usuario logado
+                usuarioLogado.seguindo++;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioLogado._id}, usuarioLogado);
+
+                //o usuario seguindo esta sendo seguido por um novo
+                //o numeto de seguidores dele tem que aumentar 
+                //adicionar um seguidor no usuario seguido
+                usuarioASerSeguido.seguidores++;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioASerSeguido._id}, usuarioASerSeguido);
+                return res.status(200).json({msg:'Usuario seguido com sucesso'});
             }
         }
         return res.status(405).json({erro :'Metodo informado nao existe'});
